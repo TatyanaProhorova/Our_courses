ff69c7c4394a79dd33a4b62be44934823ee5e83c
Tatyana Prohorova <tatyanaprohorova@gmail.com>
2021-07-05 11:25:43 +0300

    average_in_course was added

--- courses.py
+++ courses.py
@@ -103,15 +103,20 @@ class Reviewer(Mentor):
         return res
 
 
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 best_student.courses_in_progress += ['Git']
-best_student.add_courses('Введение в прогпаммирование')
+best_student.add_courses('Введение в программирование')
+
+print()
+print(best_student.courses)
+print()
 
 nice_student = Student('Vera', 'Olegova', 'your_gender')
 nice_student.courses_in_progress += ['Git']
 nice_student.courses_in_progress += ['Python']
-nice_student.add_courses('Введение в прогпаммирование')
+nice_student.add_courses('Введение в программирование')
 
 
 cool_mentor = Reviewer('Some', 'Buddy')
@@ -170,3 +175,22 @@ print(nice_student)
 print()
 print(best_student > nice_student)
 print(extra_lecturer > intel_lecturer)
+print()
+
+def average_in_course(course_id, student_list):
+    student_in_course = []
+    all_rates_in_course = []
+    for student in student_list:
+        if course_id in student.courses_in_progress + student.finished_courses:
+            student_in_course.append(student)
+            all_rates_in_course += student.grades[course_id]
+    if student_in_course == []:
+        print(f'Студенты {student_list} не проходили курс {course_id}')
+    elif len(all_rates_in_course) != 0:
+        res = sum(all_rates_in_course) / len(all_rates_in_course)
+    else:
+        res = 0
+    print(f'Средняя оценка студентов {student_in_course} за курс {course_id}: {res}')
+    return res
+
+average_in_course('Python', [best_student, nice_student])
